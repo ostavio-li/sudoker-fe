6117b20747d5ee308195c8e54194506328b90f52
lijiancheng <lijiancheng@yunzhangfang.com>
2021-12-21 16:17:40 +0800

    添加 ElementUI 和 vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// Config
+const conf = {
+    // 保存时 lint
+    lintOnSave: true,
+    // 生产环境下，console.log 显示打印位置
+    productionSourceMap: false,
+    devServer: {
+
+    }
+
+}
+
+module.exports = conf
